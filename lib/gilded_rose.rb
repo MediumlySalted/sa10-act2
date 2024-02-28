de24d1ced3a25fc27f8338025bf671983d35d7c3
Gabriel Adkins <gabea38135@yahoo.com>
2024-02-27 20:33:10 -0600

    Added code for normal_tick func to satisfy related tests

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,6 +8,10 @@ class GildedRose
   end
 
   def normal_tick
+    @days_remaining -= 1
+    return if @quality == 0
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
   end
 
   def tick
